906cbd1a5a67a434531bb62b32e01eca954d0a1a
chanidapa vantaneeyakul <ambb_bb@hotmail.com>
2017-04-24 00:30:18 +0700

    add funtion get data R415

--- sever.js
+++ sever.js
@@ -22,126 +22,12 @@ app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-
-getSw4503 ()
-function getSw4503 () {
-// os
-let standard = {}
-let sw4503 = new snmp.Session({ host: '10.77.4.1', community: 'public' })
-sw4503.get({ oid: [1,3,6,1,2,1,1,1,0] }, function (err, varbinds) {
-	standard = {
-		date: moment().format("L") +" "+ moment().format("LT"),
-		switch: 'SW4503',
-		os: varbinds[0].value
-	}
-})
-//Uptime
-sw4503.get({ oid: [1,3,6,1,2,1,1,3,0] }, function (err, varbinds) {
-/*	console.log(typeof varbinds[0].value.toString())*/
-	let timetick = varbinds[0].value
-	let min = parseInt(timetick / 6000)
-	let hour = parseInt(timetick / 360000)
-	standard.uptime = hour.toString() + " hours " + min.toString() + " min "
-})
-//CPU
-sw4503.get({ oid: [1,3,6,1,4,1,9,9,109,1,1,1,1,5,1] }, function (err, varbinds) {
-	standard.cpu = varbinds[0].value
-})
-//memory
-sw4503.get({ oid: [1,3,6,1,4,1,9,9,48,1,1,1,5,1] }, function (err, varbinds) {
-	//console.log('mem',varbinds[0].value)
-	console.log(bytesToSize(varbinds[0].value))
-	standard.mem = bytesToSize(varbinds[0].value)
-})
-//temp
-sw4503.getSubtree({ oid: [1,3,6,1,4,1,9,9,13,1,3,1,3] }, function (err, varbinds) {
-	//console.log('temp',varbinds[0].value)
-	standard.temp = varbinds[0].value
-
-})
-
-//inbound 
-var inbound = []
-
-sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,10] }, function (err, varbinds) {
-	// console.log('inbound',varbinds)
-	for (index in varbinds) {
-		let data = {
-			indexOID: varbinds[index].oid[10],
-			inbound: parseInt(varbinds[index].value/1048576)
-		}
-		inbound.push(data)
-	}
-	standard.inbound = inbound
-})
-//outbound 
-var outbound = []
-sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,16] }, function (err, varbinds) {
-	//console.log('outbound',varbinds)
-	for (index in varbinds) {
-		let data = {
-			indexOID: varbinds[index].oid[10],
-			outbound: parseInt(varbinds[index].value/1048576)
-		}
-		outbound.push(data)
-	}
-	standard.outbound = outbound
-
-})
-
-//status
-var status = []
-sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,8] }, function (err, varbinds) {
-	//console.log('status',varbinds[0].value)
-	for (index in varbinds) {
-		if(varbinds[index].value == 1){	
-    		status.push('Up') 
-		}
-		else if (varbinds[index].value == 2) {
-			status.push('Down')
-		}
-	}
-	//standard.status = status
-
-})
-//name interface 
-var interface = []
-sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,2] }, function (err, varbinds) {
-    for (index in varbinds) {
-    	interface.push(varbinds[index].value)
-	}
-    standard.interface = []
-    for (index in interface) {
-    	let data = {
-    		interface: interface[index],
-    		status: status[index],
-    		indexOID: varbinds[index].oid[10]
-    	}
-    	standard.interface.push(data)
-    }
+setTimeout(function(){ getSw4503() }, 1000)
+setTimeout(function(){ getR124()   }, 1000)
+setTimeout(function(){ getR330A()  }, 1000)
+setTimeout(function(){ getR415()   }, 1000)
 
 
- standard.total = []
- for (i in standard.interface) {
- 	for (x in standard.inbound) {
- 		if (standard.interface[i].indexOID === standard.inbound[x].indexOID) {
- 			console.log(standard.interface[i], standard.interface[i].indexOID)
- 			let item = {
- 				interface: standard.interface[i].interface,
- 				status: standard.interface[i].status,
- 				inbound: standard.inbound[x].inbound,
- 				outbound: standard.outbound[x].outbound
- 			}
- 			standard.total.push(item)
- 		}
- 	}
- }
- 	firebase.database().ref('/SW4503').push(standard)
-    sw4503.close()
-})
-}//******************************function*******************************
-
-getR124 ()
 function getR124 () {
 // os
 let standard = {}
@@ -259,7 +145,6 @@ r124.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,2] }, function (err, varbinds) {
 })
 }//******************************function*******************************
 
-getR330A ()
 function getR330A () {
 // os
 let standard = {}
@@ -376,6 +261,240 @@ r330a.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,2] }, function (err, varbinds) {
     r330a.close()
 })
 }//******************************function*******************************
+
+function getR415 () {
+// os
+let standard = {}
+let r415 = new snmp.Session({ host: '10.77.5.2', community: 'public' })
+r415.get({ oid: [1,3,6,1,2,1,1,1,0] }, function (err, varbinds) {
+	standard = {
+		date: moment().format("L") +" "+ moment().format("LT"),
+		switch: 'R415',
+		os: varbinds[0].value
+	}
+})
+//Uptime
+r415.get({ oid: [1,3,6,1,2,1,1,3,0] }, function (err, varbinds) {
+/*	console.log(typeof varbinds[0].value.toString())*/
+	let timetick = varbinds[0].value
+	let min = parseInt(timetick / 6000)
+	let hour = parseInt(timetick / 360000)
+	standard.uptime = hour.toString() + " hours " + min.toString() + " min "
+})
+//CPU
+r415.get({ oid: [1,3,6,1,4,1,9,9,109,1,1,1,1,5,1] }, function (err, varbinds) {
+	standard.cpu = varbinds[0].value
+})
+//memory
+r415.get({ oid: [1,3,6,1,4,1,9,9,48,1,1,1,5,1] }, function (err, varbinds) {
+	//console.log('mem',varbinds[0].value)
+	console.log(bytesToSize(varbinds[0].value))
+	standard.mem = bytesToSize(varbinds[0].value)
+})
+//temp
+r415.getSubtree({ oid: [1,3,6,1,4,1,9,9,13,1,3,1,3] }, function (err, varbinds) {
+	//console.log('temp',varbinds[0].value)
+	standard.temp = varbinds[0].value
+
+})
+
+//inbound 
+var inbound = []
+
+r415.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,10] }, function (err, varbinds) {
+	// console.log('inbound',varbinds)
+	for (index in varbinds) {
+		let data = {
+			indexOID: varbinds[index].oid[10],
+			inbound: parseInt(varbinds[index].value/1048576)
+		}
+		inbound.push(data)
+	}
+	standard.inbound = inbound
+})
+//outbound 
+var outbound = []
+r415.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,16] }, function (err, varbinds) {
+	//console.log('outbound',varbinds)
+	for (index in varbinds) {
+		let data = {
+			indexOID: varbinds[index].oid[10],
+			outbound: parseInt(varbinds[index].value/1048576)
+		}
+		outbound.push(data)
+	}
+	standard.outbound = outbound
+
+})
+
+//status
+var status = []
+r415.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,8] }, function (err, varbinds) {
+	//console.log('status',varbinds[0].value)
+	for (index in varbinds) {
+		if(varbinds[index].value == 1){	
+    		status.push('Up') 
+		}
+		else if (varbinds[index].value == 2) {
+			status.push('Down')
+		}
+	}
+	//standard.status = status
+
+})
+//name interface 
+var interface = []
+r415.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,2] }, function (err, varbinds) {
+    for (index in varbinds) {
+    	interface.push(varbinds[index].value)
+	}
+    standard.interface = []
+    for (index in interface) {
+    	let data = {
+    		interface: interface[index],
+    		status: status[index],
+    		indexOID: varbinds[index].oid[10]
+    	}
+    	standard.interface.push(data)
+    }
+
+
+ standard.total = []
+ for (i in standard.interface) {
+ 	for (x in standard.inbound) {
+ 		if (standard.interface[i].indexOID === standard.inbound[x].indexOID) {
+ 			console.log(standard.interface[i], standard.interface[i].indexOID)
+ 			let item = {
+ 				interface: standard.interface[i].interface,
+ 				status: standard.interface[i].status,
+ 				inbound: standard.inbound[x].inbound,
+ 				outbound: standard.outbound[x].outbound
+ 			}
+ 			standard.total.push(item)
+ 		}
+ 	}
+ }
+ 	firebase.database().ref('/R415').push(standard)
+    r415.close()
+})
+}//******************************function*******************************
+function getSw4503 () {
+// os
+let standard = {}
+let sw4503 = new snmp.Session({ host: '10.77.4.1', community: 'public' })
+sw4503.get({ oid: [1,3,6,1,2,1,1,1,0] }, function (err, varbinds) {
+	standard = {
+		date: moment().format("L") +" "+ moment().format("LT"),
+		switch: 'SW4503',
+		os: varbinds[0].value
+	}
+})
+//Uptime
+sw4503.get({ oid: [1,3,6,1,2,1,1,3,0] }, function (err, varbinds) {
+/*	console.log(typeof varbinds[0].value.toString())*/
+	let timetick = varbinds[0].value
+	let min = parseInt(timetick / 6000)
+	let hour = parseInt(timetick / 360000)
+	standard.uptime = hour.toString() + " hours " + min.toString() + " min "
+})
+//CPU
+sw4503.get({ oid: [1,3,6,1,4,1,9,9,109,1,1,1,1,5,1] }, function (err, varbinds) {
+	standard.cpu = varbinds[0].value
+})
+//memory
+sw4503.get({ oid: [1,3,6,1,4,1,9,9,48,1,1,1,5,1] }, function (err, varbinds) {
+	//console.log('mem',varbinds[0].value)
+	console.log(bytesToSize(varbinds[0].value))
+	standard.mem = bytesToSize(varbinds[0].value)
+})
+//temp
+sw4503.getSubtree({ oid: [1,3,6,1,4,1,9,9,13,1,3,1,3] }, function (err, varbinds) {
+	//console.log('temp',varbinds[0].value)
+	standard.temp = varbinds[0].value
+
+})
+
+//inbound 
+var inbound = []
+
+sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,10] }, function (err, varbinds) {
+	// console.log('inbound',varbinds)
+	for (index in varbinds) {
+		let data = {
+			indexOID: varbinds[index].oid[10],
+			inbound: parseInt(varbinds[index].value/1048576)
+		}
+		inbound.push(data)
+	}
+	standard.inbound = inbound
+})
+//outbound 
+var outbound = []
+sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,16] }, function (err, varbinds) {
+	//console.log('outbound',varbinds)
+	for (index in varbinds) {
+		let data = {
+			indexOID: varbinds[index].oid[10],
+			outbound: parseInt(varbinds[index].value/1048576)
+		}
+		outbound.push(data)
+	}
+	standard.outbound = outbound
+
+})
+
+//status
+var status = []
+sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,8] }, function (err, varbinds) {
+	//console.log('status',varbinds[0].value)
+	for (index in varbinds) {
+		if(varbinds[index].value == 1){	
+    		status.push('Up') 
+		}
+		else if (varbinds[index].value == 2) {
+			status.push('Down')
+		}
+	}
+	//standard.status = status
+
+})
+//name interface 
+var interface = []
+sw4503.getSubtree({ oid: [1,3,6,1,2,1,2,2,1,2] }, function (err, varbinds) {
+    for (index in varbinds) {
+    	interface.push(varbinds[index].value)
+	}
+    standard.interface = []
+    for (index in interface) {
+    	let data = {
+    		interface: interface[index],
+    		status: status[index],
+    		indexOID: varbinds[index].oid[10]
+    	}
+    	standard.interface.push(data)
+    }
+
+
+ standard.total = []
+ for (i in standard.interface) {
+ 	for (x in standard.inbound) {
+ 		if (standard.interface[i].indexOID === standard.inbound[x].indexOID) {
+ 			console.log(standard.interface[i], standard.interface[i].indexOID)
+ 			let item = {
+ 				interface: standard.interface[i].interface,
+ 				status: standard.interface[i].status,
+ 				inbound: standard.inbound[x].inbound,
+ 				outbound: standard.outbound[x].outbound
+ 			}
+ 			standard.total.push(item)
+ 		}
+ 	}
+ }
+ 	firebase.database().ref('/SW4503').push(standard)
+    sw4503.close()
+})
+}//******************************function*******************************
+
 
 
 function bytesToSize(bytes) {
